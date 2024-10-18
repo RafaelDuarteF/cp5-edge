33dda52fb733923275308d797ee4a350c7063646
Rafael Duarte <103393497+RafaelDuarteF@users.noreply.github.com>
2024-10-18 20:06:49 -0300

    Update FE-Club_IoT - ESP32Config.ino

--- FE-Club_IoT - ESP32Config.ino
+++ FE-Club_IoT - ESP32Config.ino
@@ -1,20 +1,13 @@
-//Autor: Fábio Henrique Cabrini
-//Resumo: Esse programa possibilita ligar e desligar o led onboard, além de mandar o status para o Broker MQTT possibilitando o Helix saber
-//se o led está ligado ou desligado.
-//Revisões:
-//Rev1: 26-08-2023 Código portado para o ESP32 e para realizar a leitura de luminosidade e publicar o valor em um tópico aprorpiado do broker 
-//Autor Rev1: Lucas Demetrius Augusto 
-//Rev2: 28-08-2023 Ajustes para o funcionamento no FIWARE Descomplicado
-//Autor Rev2: Fábio Henrique Cabrini
-//Rev3: 1-11-2023 Refinamento do código e ajustes para o funcionamento no FIWARE Descomplicado
-//Autor Rev3: Fábio Henrique Cabrini
 #include <WiFi.h>
 #include <PubSubClient.h>
+
+#include <Adafruit_Sensor.h>                       // Biblioteca DHT Sensor Adafruit 
 #include <DHT.h>
+#include <DHT_U.h>
 
 // Configurações - variáveis editáveis
-const char* default_SSID = "Wokwi-GUEST"; // Nome da rede Wi-Fi, importante a sua rede wi-fi ter suporte a dispositivos de 2,4GHz devido ao ESP32 utilizar essa frequência 
-const char* default_PASSWORD = ""; // Senha da rede Wi-Fi
+const char* default_SSID = "FIAP-IBM"; // Nome da rede Wi-Fi, importante a sua rede wi-fi ter suporte a dispositivos de 2,4GHz devido ao ESP32 utilizar essa frequência 
+const char* default_PASSWORD = "Challenge@24!"; // Senha da rede Wi-Fi
 const char* default_BROKER_MQTT = "191.232.172.130"; // IP do Broker MQTT - IP da VM da Azure
 const int default_BROKER_PORT = 1883; // Porta do Broker MQTT
 const char* default_TOPICO_SUBSCRIBE = "/TEF/device010/cmd"; // Tópico MQTT de escuta
@@ -36,18 +29,19 @@ char* TOPICO_PUBLISH_2 = const_cast<char*>(default_TOPICO_PUBLISH_2);
 char* TOPICO_PUBLISH_3 = const_cast<char*>(default_TOPICO_PUBLISH_3);
 char* ID_MQTT = const_cast<char*>(default_ID_MQTT);
 
-//Variáveis dos componentes
-#define DHTTYPE DHT22
-#define DHTPIN  4
-DHT dht(DHTPIN, DHTTYPE);
+
+
+// selecione um sensor, retirando o comentário - duas barras
+#define DHTTYPE    DHT11                           // Sensor DHT11
+//#define DHTTYPE      DHT22                       // Sensor DHT22 ou AM2302
+#define DHTPIN 4                                   // Pino do Arduino conectado no Sensor(Data) 
+DHT_Unified dht(DHTPIN, DHTTYPE);                  // configurando o Sensor DHT - pino e tipo
+uint32_t delayMS;                                  // variável para atraso no tempo
 
 WiFiClient espClient;
 PubSubClient MQTT(espClient);
 char EstadoSaida = '0';
 
-void initSerial() {
-    Serial.begin(115200);
-}
 
 void initWiFi() {
     delay(10);
@@ -62,19 +56,69 @@ void initMQTT() {
     MQTT.setServer(BROKER_MQTT, BROKER_PORT);
 }
 
-void setup() {
-    initSerial();
-    initWiFi();
-    initMQTT();
-    delay(5000);
+void setup()
+{
+  Serial.begin(115200);  
+  initWiFi();
+  initMQTT();
+  delay(5000);                          // monitor serial 9600 bps
+  dht.begin();                                    // inicializa a função
+  Serial.println("Usando o Sensor DHT");
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);           // imprime os detalhes do Sensor de Temperatura
+  Serial.println("------------------------------------");
+  Serial.println("Temperatura");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Valor max:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Valor min:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolucao:   "); Serial.print(sensor.resolution); Serial.println(" *C");
+  Serial.println("------------------------------------");
+  dht.humidity().getSensor(&sensor);            // imprime os detalhes do Sensor de Umidade
+  Serial.println("------------------------------------");
+  Serial.println("Umidade");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Valor max:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Valor min:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolucao:   "); Serial.print(sensor.resolution); Serial.println("%");
+  Serial.println("------------------------------------");
+  delayMS = sensor.min_delay / 1000;   
+        // define o atraso entre as leituras
 }
-
-void loop() {
-    VerificaConexoesWiFIEMQTT();
-    handleTemperatureandHumidity(); // Função para ler e escrever a temperatura e humidade
-    handleLuminosity(); // Função para ler e escrever a luminosidade
-    MQTT.loop();
-    delay(1000);
+void loop()
+{
+  VerificaConexoesWiFIEMQTT();
+  handleLuminosity(); // Função para ler e escrever a luminosidade
+  MQTT.loop();
+  delay(delayMS);                               // atraso entre as medições
+  sensors_event_t event;                        // inicializa o evento da Temperatura
+  dht.temperature().getEvent(&event);           // faz a leitura da Temperatura
+  if (isnan(event.temperature))                 // se algum erro na leitura
+  {
+    Serial.println("Erro na leitura da Temperatura!");
+  }
+  else                                          // senão
+  {
+    Serial.print("Temperatura: ");              // imprime a Temperatura
+    Serial.print(event.temperature);
+    float temperature = event.temperature;
+    String temp = String(temperature);
+    MQTT.publish(TOPICO_PUBLISH_1, temp.c_str());
+    Serial.println(" *C");
+  }
+  dht.humidity().getEvent(&event);              // faz a leitura de umidade
+  if (isnan(event.relative_humidity))           // se algum erro na leitura
+  {
+    Serial.println("Erro na leitura da Umidade!");
+  }
+  else                                          // senão
+  {
+    Serial.print("Umidade: ");                  // imprime a Umidade
+    Serial.print(event.relative_humidity);
+    float humidade = event.relative_humidity;
+    String hum = String(humidade);
+    MQTT.publish(TOPICO_PUBLISH_2, hum.c_str());
+    Serial.println("%");
+  }
 }
 
 void reconectWiFi() {
@@ -111,17 +155,6 @@ void reconnectMQTT() {
             delay(2000);
         }
     }
-}
-
-void handleTemperatureandHumidity() {
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    String temp = String(temperature);
-    String hum = String(humidity);
-    Serial.println("Temperatura: " + temp + "°C");
-    Serial.println("Humidade: " + hum + "%");
-    MQTT.publish(TOPICO_PUBLISH_1, temp.c_str());
-    MQTT.publish(TOPICO_PUBLISH_2, hum.c_str());
 }
 
 void handleLuminosity() {
